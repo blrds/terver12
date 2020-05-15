0b7979e52d9f3c6f4ca2cb1df5f17bfa74ee8359
Vladislav <Vladislav@DESKTOP-60B1S6U>
2020-05-15 15:31:24 +0700

    first upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.AccessibleRole = System.Windows.Forms.AccessibleRole.Alert;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(153, 12);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Распред";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Color = System.Drawing.Color.Red;
+            series2.Legend = "Legend1";
+            series2.Name = "Точки";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1436, 613);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            title1.BorderWidth = 5;
+            title1.Name = "Title1";
+            title1.Text = "Логнормальное распределение";
+            this.chart1.Titles.Add(title1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Построить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "От";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "До";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(135, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 103);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(135, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 142);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(135, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 181);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(135, 20);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox5_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Количсетво";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Мат Ожидание";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Среднеквадратичное";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 207);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(101, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Запись в файл";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 46);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Загрузить из файла";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1601, 637);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,250 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public class myPoint {
+            public double x { get; set; }
+            public double y{ get; set; }
+            public string toString() {
+                return x.ToString() + " " + y.ToString();
+            }
+           
+        }
+
+        public sealed class myPointMap : ClassMap<myPoint>
+        {
+            public myPointMap()
+            {
+                Map(m => m.x).Index(0);
+                Map(m => m.y).Index(1);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private String openFileName, saveFileName;
+
+        public static void Log(string message)
+        {
+            File.AppendAllText("log.txt", message+"\n");
+        }
+
+        private double fx(double x, double m, double q) {
+            return Math.Exp(-Math.Pow(Math.Log(x) - m, 2) / (2 * q * q)) / (x * q * Math.Sqrt(2 * Math.PI));            
+        }
+
+        private void generate(double from, double to, double m, double q, out double K1, out double K2) { 
+            Random random = new Random();
+            double k1, k2;
+            do
+            {
+                k1 = random.NextDouble();
+                k2 = random.NextDouble();
+                K1 = from + (to - from) * k1;
+                K2 = k2 * fx(Math.Exp(m - q * q),m, q);
+                Log("generated " + K1.ToString() + " " + K2.ToString());
+            } while (K2 > fx(K1, m, q));
+        }
+
+        private void drawGraph(double from, double to, double m, double q) {
+            const double MOVE = 0.05;
+            double x = from;
+            this.chart1.Series[0].Points.Clear();
+            while (x <= to)
+            {
+                this.chart1.Series[0].Points.AddXY(x, fx(x, m, q));
+                x += MOVE;
+            }
+        }
+        
+        private void drawPoints(List<myPoint> list)
+        {
+            this.chart1.Series[1].Points.Clear();
+            Log("cleared");
+            foreach (myPoint mp in list) {
+                this.chart1.Series[1].Points.AddXY(mp.x, mp.y);
+                Log("drawn " + mp.ToString());
+                this.chart1.Update();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double from = Convert.ToDouble(this.textBox1.Text), to = Convert.ToDouble(this.textBox2.Text);
+            double m = Convert.ToDouble(this.textBox4.Text), q = Convert.ToDouble(this.textBox5.Text);
+            double x = from, X, Y;
+            int count = Convert.ToInt16(this.textBox3.Text);
+            bool toFile = this.checkBox1.Checked;
+            if (File.Exists(saveFileName)) File.Delete(saveFileName);
+            drawGraph(from, to, m, q);
+            this.chart1.Series[1].Points.Clear();
+            List<myPoint> list = new List<myPoint> {};
+            for (int i = 0; i < count; i++)
+            {
+                generate(from, to, m, q, out X, out Y);
+                Log("postgenerated" + X.ToString() + " " + Y.ToString());
+                list.Add(new myPoint() { x = X, y = Y });
+                Log("added" + list[list.Count - 1].toString());
+            }
+            drawPoints(list);
+            if (toFile) {
+
+                using (StreamWriter streamWriter = new StreamWriter(saveFileName))
+                {
+
+                    using (CsvWriter csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
+                    {
+
+                        csvWriter.Configuration.Delimiter = ";";
+                        csvWriter.WriteRecords(list);
+                    }
+
+                }
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != ','))
+            {
+                e.Handled = true;
+            }
+            if ((e.KeyChar == ',') && ((sender as TextBox).Text.IndexOf(',') > -1))
+            {
+                e.Handled = true;
+            }
+            if (Char.IsControl(e.KeyChar))
+                if (e.KeyChar == (char)Keys.Enter)
+                   textBox2.Focus();
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != ','))
+            {
+                e.Handled = true;
+            }
+            if ((e.KeyChar == ',') && ((sender as TextBox).Text.IndexOf(',') > -1))
+            {
+                e.Handled = true;
+            }
+            if (Char.IsControl(e.KeyChar))
+                if (e.KeyChar == (char)Keys.Enter)
+                   textBox3.Focus();
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            if (Char.IsControl(e.KeyChar))
+                if (e.KeyChar == (char)Keys.Enter)
+                    textBox4.Focus();
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != ',') && (e.KeyChar != '-'))
+            {
+                e.Handled = true;
+            }
+            if ((e.KeyChar == ',') && ((sender as TextBox).Text.IndexOf(',') > -1))
+            {
+                e.Handled = true;
+            }
+            if ((e.KeyChar == '-') && ((sender as TextBox).Text.IndexOf('-') > -1))
+            {
+                e.Handled = true;
+            }
+            if (Char.IsControl(e.KeyChar))
+                if (e.KeyChar == (char)Keys.Enter)
+                    textBox5.Focus();
+        }
+
+        private void textBox5_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != ',') && (e.KeyChar != '-'))
+            {
+                e.Handled = true;
+            }
+            if ((e.KeyChar == ',') && ((sender as TextBox).Text.IndexOf(',') > -1))
+            {
+                e.Handled = true;
+            }
+            if (Char.IsControl(e.KeyChar))
+                if (e.KeyChar == (char)Keys.Enter)
+                    button1.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try {
+                File.Delete("log.txt");
+            } catch (Exception err) { };
+        }
+
+        private int ComparisonbyX(myPoint a, myPoint b)
+        {
+            if (a.x == b.x) return 0;
+            if (a.x > b.x) return 1;
+            else return -1;
+        }
+
+        private int ComparisonbyY(myPoint a, myPoint b)
+        {
+            if (a.y == b.y) return 0;
+            if (a.y > b.y) return 1;
+            else return -1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                openFileName = openFileDialog.FileName;
+                List<myPoint> list=new List<myPoint> { };
+                using (StreamReader streamReader = new StreamReader(openFileName))
+                {
+                    using (CsvReader csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+                    {
+                        csvReader.Configuration.Delimiter = ";";
+                        csvReader.Configuration.RegisterClassMap<myPointMap>();
+                        while (csvReader.Read())
+                            list.Add(csvReader.GetRecord<myPoint>());
+                    }
+                }
+                drawPoints(list);   
+                list.Sort(ComparisonbyX);
+                label1.Text = Convert.ToString(list[0].x);
+                label2.Text = Convert.ToString(list[list.Count-1].x);
+
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                SaveFileDialog saveFileDiolog = new SaveFileDialog();
+                saveFileDiolog.Filter = "csv files (*.csv)|*.csv";
+                if (saveFileDiolog.ShowDialog() == DialogResult.OK)
+                    saveFileName = saveFileDiolog.FileName;
+
+            }
+        }
+    }
+}
